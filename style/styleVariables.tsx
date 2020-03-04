babc34d2a743359696c8668418ec6a185b2617db
Joanna Graczyk <joanna.graczyk1@gmail.com>
2020-03-04 20:54:22 +0100

    smth more

--- style/styleVariables.tsx
+++ style/styleVariables.tsx
@@ -2,7 +2,8 @@ export const colors = {
   first: '#59B6AE',
   second: '#4D3771',
   third: '#CF4160',
-  fourth: '#FFDB68',
+  fourth: 'orange',
+  //fourth: '#FFDB68',
   light: 'white',
   dark: 'grey'
 };
