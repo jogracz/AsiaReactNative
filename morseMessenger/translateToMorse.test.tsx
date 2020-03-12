df59910a30f047a31067fd7ed0a607e8f0ee16b8
Joanna Graczyk <joanna.graczyk1@gmail.com>
2020-03-12 14:48:48 +0100

    more tests

--- morseMessenger/translateToMorse.test.tsx
+++ morseMessenger/translateToMorse.test.tsx
@@ -5,46 +5,16 @@ const s = DictOptions.s;
 const l = DictOptions.l;
 const pause = DictOptions.pause;
 
-test('translate "a"', () => {
-  expect(translateToMorse('a')).toStrictEqual([s, l, pause]);
+test('translate ""', () => {
+  expect(translateToMorse('')).toStrictEqual([]);
 });
 
-test('translate "What a day"', () => {
-  expect(translateToMorse('What a day')).toStrictEqual([
-    s,
-    l,
-    l,
-    pause,
-    s,
-    s,
-    s,
-    s,
-    pause,
-    s,
-    l,
-    pause,
-    l,
-    pause,
-    pause,
-    pause,
-    s,
-    l,
-    pause,
-    pause,
-    pause,
-    l,
-    s,
-    s,
-    pause,
-    s,
-    l,
-    pause,
-    l,
-    s,
-    l,
-    l,
-    pause
-  ]);
+test('translate " "', () => {
+  expect(translateToMorse(' ')).toStrictEqual([pause, pause]);
+});
+
+test('translate "a"', () => {
+  expect(translateToMorse('a')).toStrictEqual([s, l, pause]);
 });
 
 test("translate 'Abrakadabra'", () => {
@@ -88,6 +58,116 @@ test("translate 'Abrakadabra'", () => {
     s,
     pause,
     s,
+    l,
+    pause
+  ]);
+});
+
+test('translate "What a day"', () => {
+  expect(translateToMorse('What a day')).toStrictEqual([
+    s,
+    l,
+    l,
+    pause,
+    s,
+    s,
+    s,
+    s,
+    pause,
+    s,
+    l,
+    pause,
+    l,
+    pause,
+    pause,
+    pause,
+    s,
+    l,
+    pause,
+    pause,
+    pause,
+    l,
+    s,
+    s,
+    pause,
+    s,
+    l,
+    pause,
+    l,
+    s,
+    l,
+    l,
+    pause
+  ]);
+});
+
+test('translate "!?."', () => {
+  expect(translateToMorse('!?.')).toStrictEqual([]);
+});
+
+test('translate "ah! !? ok."', () => {
+  expect(translateToMorse('ah! !? ok.')).toStrictEqual([
+    s,
+    l,
+    pause,
+    s,
+    s,
+    s,
+    s,
+    pause,
+    pause,
+    pause,
+    pause,
+    pause,
+    l,
+    l,
+    l,
+    pause,
+    l,
+    s,
+    l,
+    pause
+  ]);
+});
+
+test('translate "490 521"', () => {
+  expect(translateToMorse('490 521')).toStrictEqual([
+    s,
+    s,
+    s,
+    s,
+    l,
+    pause,
+    l,
+    l,
+    l,
+    l,
+    s,
+    pause,
+    l,
+    l,
+    l,
+    l,
+    l,
+    pause,
+    pause,
+    pause,
+    s,
+    s,
+    s,
+    s,
+    s,
+    pause,
+    s,
+    s,
+    l,
+    l,
+    l,
+    pause,
+    s,
+    l,
+    l,
+    l,
     l,
     pause
   ]);
--- morseMessenger/translateToMorse.tsx
+++ morseMessenger/translateToMorse.tsx
@@ -1,10 +1,13 @@
 import { morseDictionary, DictOptions } from './morseDictionary';
 
 export default function translateToMorse(sentence: string) {
+  if (!sentence) return [];
   const letters = sentence.toLowerCase().split('');
   const morseCode: Array<string> = [];
-  letters.forEach((letter: string) =>
-    morseCode.push(...morseDictionary[letter], DictOptions.pause)
-  );
+  letters.forEach((letter: string) => {
+    if (morseDictionary[letter]) {
+      morseCode.push(...morseDictionary[letter], DictOptions.pause);
+    }
+  });
   return morseCode;
 }
