ce2e5023406a5343e6e97646789cf0be9cd05fee
Joanna Graczyk <joanna.graczyk1@gmail.com>
2020-03-12 13:23:52 +0100

    small

--- morseMessenger/flashHook.tsx
+++ morseMessenger/flashHook.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import { DictOptions } from './morseDictionary';
 import translateToMorse from './translateToMorse';
 
